33559c15a363a3da273c74c3c20e1f1eec7799c1
hafsid <50413142+hafsid@users.noreply.github.com>
2021-06-29 17:00:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+  
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    
+  }
+
+  drawSprites();
+}
+
+
+
+
